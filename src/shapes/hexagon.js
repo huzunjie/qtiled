c6eab91b72229f6b0344ebe00393aa9a3c562367
huzunjie <huzunjie@360.cn>
2021-08-10 22:52:18 +0800

    Pathfinding related content updated archive

--- src/shapes/hexagon.js
+++ src/shapes/hexagon.js
@@ -1,9 +1,20 @@
 /* 正六边形地图元件方法 */
 
-import { HALF, FLAH, QUAR, RAUQ, TQUA, getPolygonVertexes, getPolygonPositions, getPolygonInfoByPos } from './polygon';
+import {
+  HALF,
+  FLAH,
+  QUAR,
+  RAUQ,
+  TQUA as TQUA_VAL,
+  isStaggerLine,
+  getVertexes as getPolygonVertexes,
+  getPosition as getPolygonPosition,
+  getPositions as getPolygonPositions,
+  getInfoByPos as getPolygonInfoByPos,
+} from './polygon';
 
 // 宽高为1的六边形顶点集合（上下为尖）
-const hexagonVertexes = [
+export const vertexes = [
   [0, FLAH],
   [HALF, RAUQ],
   [HALF, QUAR],
@@ -12,15 +23,47 @@ const hexagonVertexes = [
   [FLAH, RAUQ],
 ];
 
+export const TQUA = TQUA_VAL;
+
+/* 左上、右上、右下、左下、左边、右边，6个边邻居 [xNum, yNum, cost, angle] 差值及距离成本及渲染角度 */
+export const directionsNormal = [
+  [-1, -1, 1, '↖'],
+  [0, -1, 1, '↗'],
+  [0, 1, 1, '↘'],
+  [-1, 1, 1, '↙'],
+  [-1, 0, 1, '←'],
+  [1, 0, 1, '→'],
+];
+
+/* 错列行邻居下标差值 */
+export const directionsOffset = [
+  [0, -1, 1, '↖'],
+  [1, -1, 1, '↗'],
+  [1, 1, 1, '↘'],
+  [0, 1, 1, '↙'],
+  [-1, 0, 1, '←'],
+  [1, 0, 1, '→'],
+];
+
 /* 根据计划渲染的六边形宽高值得到顶点坐标集
-* @param  {Number}  width    宽度
-* @param  {Number}  height   高度
+* @param  {Array}   size    如： [width{Number}, height{Number}]
 * @param  {String}  axis     主轴方向 'x' || 'y'；默认为 'y'，上下是尖
 * @return {Array}   [[x, y], ...]
 */
-export function getHexagonVertexes(width = 1, height = 1, axis = 'y') {
-  return getPolygonVertexes(hexagonVertexes, width, height, axis);
+export function getVertexes([width = 1, height = 1] = [1, 1], axis = 'y') {
+  return getPolygonVertexes(vertexes, width, height, axis);
 };
+
+/* 得到一个错列布局六边形地图瓦片的坐标位置
+ * @param  {Array}   xyNum          目标元素XY索引值，如：[0, 0]
+ * @param  {Array}   tileSize       单瓦片图宽高值，如：[80, 40]
+ * @param  {String}  stagger        需要错位排列的行：['odd', 'even', 'none']；默认为 'odd' 奇数行错开（通常第一行是0行）
+ * @param  {Array}   originXY       原点像素坐标值，如：[0, 0]
+ * @return {Array}   [x, y]
+ */
+export function getPosition(xyNum = [0, 0], tileSize = [8, 4], stagger = 'odd', originXY = [0, 0]) {
+  return getPolygonPosition(TQUA, xyNum, tileSize, stagger, originXY);
+}
 
 /* 得到一组错列布局六边形地图瓦片的坐标偏移位置集合
  * @param  {Array}   mainAxisRange  主轴行序号区间，如：[0, 0]
@@ -30,7 +73,7 @@ export function getHexagonVertexes(width = 1, height = 1, axis = 'y') {
  * @param  {String}  stagger        需要错列的行：['odd', 'even', 'none']；默认为 'odd' 奇数行错开（通常第一行是0行）
  * @return {Array}   [[x, y], ...]
  */
-export function getHexagonPositions(mainAxisRange = [0, 0], subAxisRange = [0, 0], tileSize = [8, 4], renderOrder = 'RightDown', stagger = 'odd') {
+export function getPositions(mainAxisRange = [0, 0], subAxisRange = [0, 0], tileSize = [8, 4], renderOrder = 'RightDown', stagger = 'odd') {
   return getPolygonPositions(TQUA, mainAxisRange, subAxisRange, tileSize, stagger, renderOrder);
 }
 
@@ -39,8 +82,19 @@ export function getHexagonPositions(mainAxisRange = [0, 0], subAxisRange = [0, 0
  * @param  {Array}   originPos      地图起点元素渲染时像素坐标值，如：[x<Number>, y<Number>]
  * @param  {Array}   tileSize       单瓦片图宽高值，如：[80, 40]
  * @param  {String}  stagger        需要错列的行：['odd', 'even', 'none']；默认为 'odd' 奇数行错开（通常第一行是0行）
- * @return {Object}  {xNum, yNum, x, y}
+ * @return {Array}  [xNum, yNum, x, y]
  */
-export function getHexagonInfoByPos(pos = [0, 0], originPos = [0, 0], tileSize = [8, 4], stagger = 'odd') {
+export function getInfoByPos(pos = [0, 0], originPos = [0, 0], tileSize = [8, 4], stagger = 'odd') {
   return getPolygonInfoByPos(TQUA, pos, originPos, tileSize, stagger);
+}
+
+/* 获得指定tile下标周边紧邻的邻居们
+ * @param  {Array}     originXyNum     参考点元素下标，如：[0, 0]
+ * @param  {String}    stagger         需要错位排列的行：['odd', 'even', 'none']；默认为 'odd' 奇数行错开（通常第一行是0行）
+ * @return {Array}  [[xNum, yNum]]
+ */
+export function getNeighbors(originXyNum = [0, 0], stagger = 'odd') {
+  const [originXNum, originYNum] = originXyNum;
+  const directions = isStaggerLine(originYNum, stagger) ? directionsOffset : directionsNormal;
+  return directions.map(([xNum, yNum]) => [xNum + originXNum, yNum + originYNum]);
 }
--- src/shapes/rhombus.js
+++ src/shapes/rhombus.js
@@ -1,22 +1,37 @@
 /* 正菱形地图元件方法 */
+// 点(x, y)在宽w高h的菱形内：| x * h | + | y * w | < h * w * 0.5
 
-import { HALF, FLAH, getPolygonVertexes, getPolygonPositions, getPolygonInfoByPos, twoDimForEach } from './polygon';
+import {
+  HALF,
+  FLAH,
+  twoDimForEach,
+  getVertexes as getPolygonVertexes,
+  getPositions as getPolygonPositions,
+  getInfoByPos as getPolygonInfoByPos,
+} from './polygon';
 
 // 宽高为1的正菱形顶点集合
-const rhombusVertexes = [
+const vertexes = [
   [0, FLAH],
   [HALF, 0],
   [0, HALF],
   [FLAH, 0],
 ];
 
+/* 获取宽高的一半（菱形中心点在顶点坐标系中的值）
+* @param  {Array}   size    如： [width{Number}, height{Number}]
+* @return {Array}   [halfWidth, halfHeight]
+*/
+export function getHalfSize([width = 1, height = 1] = [1, 1]) {
+  return [width * HALF, height * HALF];
+}
+
 /* 根据计划渲染的菱形宽高值，得到顶点坐标集合
-* @param  {Number}  width    宽度
-* @param  {Number}  height   高度
+* @param  {Array}   size    如： [width{Number}, height{Number}]
 * @return {Array}   [[x, y], ...]
 */
-export function getRhombusVertexes(width = 1, height = 1) {
-  return getPolygonVertexes(rhombusVertexes, width, height);
+export function getVertexes([width = 1, height = 1] = [1, 1]) {
+  return getPolygonVertexes(vertexes, width, height);
 };
 
 /* 得到一组错列布局菱形地图瓦片的坐标偏移位置集合
@@ -27,12 +42,12 @@ export function getRhombusVertexes(width = 1, height = 1) {
  * @param  {String}  renderOrder    渲染方向：['RightDown','RightUp', 'LeftDown', 'LeftUp']；默认为 'RightDown'
  * @return {Array}   [[x, y], ...]
  */
-export function getRhombusPositions(mainAxisRange = [0, 0], subAxisRange = [0, 0], tileSize = [8, 4], renderOrder = 'RightDown', stagger = 'odd') {
+export function getPositions(mainAxisRange = [0, 0], subAxisRange = [0, 0], tileSize = [8, 4], renderOrder = 'RightDown', stagger = 'odd') {
   return getPolygonPositions(HALF, mainAxisRange, subAxisRange, tileSize, stagger, renderOrder);
 }
 
 /* 按等距布局菱形单元横纵坐标值及单元格宽高得到渲染坐标值 */
-export function getIsometricXY(xNum, yNum, halfWidth, halfHeight) {
+export function getIsometricPosition(xNum, yNum, halfWidth, halfHeight) {
   return [(xNum + yNum) * halfWidth, (yNum - xNum) * halfHeight];
 }
 
@@ -44,11 +59,10 @@ export function getIsometricXY(xNum, yNum, halfWidth, halfHeight) {
  * @param  {String}  renderOrder    渲染方向：['RightDown','RightUp', 'LeftDown', 'LeftUp']；默认为 'RightDown'
  * @return {Array}   [[x, y]...]
  */
-export function getIsometricRhombusPositions(mainAxisRange = [0, 0], subAxisRange = [0, 0], tileSize = [8, 4], renderOrder = 'RightDown') {
-  const halfWidth = tileSize[0] / 2;
-  const halfHeight = tileSize[1] / 2;
+export function getIsometricPositions(mainAxisRange = [0, 0], subAxisRange = [0, 0], tileSize = [8, 4], renderOrder = 'RightDown') {
+  const [halfWidth, halfHeight] = getHalfSize(tileSize);
   return twoDimForEach(mainAxisRange, subAxisRange, renderOrder, (xNum, yNum) => [
-    ...getIsometricXY(xNum, yNum, halfWidth, halfHeight),
+    ...getIsometricPosition(xNum, yNum, halfWidth, halfHeight),
     xNum,
     yNum,
   ]);
@@ -59,9 +73,9 @@ export function getIsometricRhombusPositions(mainAxisRange = [0, 0], subAxisRang
  * @param  {Array}   originPos      地图起点元素渲染时像素坐标值，如：[x<Number>, y<Number>]
  * @param  {Array}   tileSize       单瓦片图宽高值，如：[80, 40]
  * @param  {String}  stagger        需要错列的行：['odd', 'even', 'none']；默认为 'odd' 奇数行错开（通常第一行是0行）
- * @return {Object}  {xNum, yNum, x, y}
+ * @return {Array}  [xNum, yNum, x, y]
  */
-export function getRhombusInfoByPos(pos = [0, 0], originPos = [0, 0], tileSize = [8, 4], stagger = 'odd') {
+export function getInfoByPos(pos = [0, 0], originPos = [0, 0], tileSize = [8, 4], stagger = 'odd') {
   return getPolygonInfoByPos(HALF, pos, originPos, tileSize, stagger);
 }
 
@@ -71,15 +85,14 @@ export function getRhombusInfoByPos(pos = [0, 0], originPos = [0, 0], tileSize =
  * @param  {Array}   tileSize       单瓦片图宽高值，如：[80, 40]
  * @return {Object}  {xNum, yNum, x, y}
  */
-export function getIsometricRhombusInfoByPos(pos = [0, 0], originPos = [0, 0], tileSize = [8, 4]) {
-  const halfHeight = tileSize[1] * HALF;
-  const halfWidth = tileSize[0] * HALF;
+export function getIsometricInfoByPos(pos = [0, 0], originPos = [0, 0], tileSize = [8, 4]) {
+  const [halfWidth, halfHeight] = getHalfSize(tileSize);
   const [originX, originY] = originPos;
   const xSteps = (pos[0] - originX) / halfWidth * HALF;
   const ySteps = (pos[1] - originY) / halfHeight * HALF;
   const yNum = Math.round(ySteps + xSteps);
   const xNum = Math.round(xSteps - ySteps);
-  const [x, y] = getIsometricXY(xNum, yNum, halfWidth, halfHeight);
+  const [x, y] = getIsometricPosition(xNum, yNum, halfWidth, halfHeight);
   return [
     xNum,
     yNum,
